a3bd913dd5d5e2131af5f62cb15a957bf7d24bcb
Hamza Ahmed <hamzaahmed@Hamzas-Mac-mini.local>
2023-05-16 00:47:48 +0800

    added customised css

--- src/modules/UniversitySearchResults/UniversityItem/index.tsx
+++ src/modules/UniversitySearchResults/UniversityItem/index.tsx
@@ -1,15 +1,5 @@
 import React from 'react';
-import {
-  CardWrapper,
-  CardImage,
-  CardTextWrapper,
-  CardTextTitle,
-  CardTextBody,
-  CardStatWrapper,
-  CardStats,
-  LinkText,
-} from './style';
-import { Box, Flex, Text } from '../../../ui-components';
+import { Flex, Text } from '../../../ui-components';
 
 const url = require('../../../images/university.jpg')
 
@@ -28,20 +18,32 @@ const UniversityItem: React.FC<CardsProps> = ({ List }) => {
     const { name, country, web_pages } = List;
 
     return (
-      <Box p={3} data-testid="university-item">
-        <img height={180} src={url} alt="university image" />
-        <Flex>
-          <Text>{name}</Text>
-          <Text>{country}</Text>
-        </Flex>
-        <Flex>
-          <Flex>
-            <Text target="blank" href={web_pages[0]}>
-              {web_pages[0]}
-            </Text>
+      <Flex 
+        justifyContent="space-between" 
+        flexDirection='column' 
+        alignItems='space-between' 
+        p={3} 
+        m={3} 
+        width='15em' 
+        height='20em' 
+        border="solid" 
+        data-testid="university-item">
+          <img 
+            height="200px" 
+            src={url} 
+            alt="university image" />
+          <Flex justifyContent="center">
+            <Text fontFamily='mono'>{name}</Text>
+            <Text fontFamily='mono'>{country}</Text>
           </Flex>
-        </Flex>
-      </Box>
+          <Flex justifyContent="center">
+            <Flex >
+              <a target="blank" href={web_pages[0]}>
+                {web_pages[0]}
+              </a>
+            </Flex>
+         </Flex>
+       </Flex>
     );
   } else {
     return null;
